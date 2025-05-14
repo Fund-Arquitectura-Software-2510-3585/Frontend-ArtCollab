38e4f04be8aad2be0a80d0985a477c755c9c18ed
diegooo01 <129528729+diegooo01@users.noreply.github.com>
2025-05-14 08:36:51 -0500

    feat: Images changed

--- src/app/content/pages/home/home.component.ts
+++ src/app/content/pages/home/home.component.ts
@@ -28,7 +28,7 @@ export class HomeComponent  implements OnInit {
       author: 'María López',
       date: '10/05/2025',
       applicationDate: '12/05/2025',
-      imageSrc: 'https://fundacionjuanjotorrejon.org/wp-content/uploads/2020/06/cuentos-1080x675.jpg',
+      imageSrc: 'https://s1.1zoom.me/b5050/281/Fantastic_world_Tigers_Magical_animals_518194_1920x1080.jpg',
       description: 'Este proyecto trata sobre crear cuentos ilustrados con animales fantásticos que enseñen valores a los niños. A través de personajes mágicos y situaciones sorprendentes, se busca estimular la creatividad y la imaginación infantil. Además, se abordarán temas como la empatía, el respeto por la diversidad y la importancia de la amistad en un formato lúdico y educativo que permitirá a los niños aprender mientras se divierten.'
     },
     {
@@ -37,7 +37,7 @@ export class HomeComponent  implements OnInit {
       author: 'Carlos Pérez',
       date: '11/05/2025',
       applicationDate: '12/05/2025',
-      imageSrc: 'https://vocalialogopedia.com/wp-content/uploads/2022/11/3583238-scaled.jpg',
+      imageSrc: 'https://img.freepik.com/vector-gratis/fondo-degradado-celebracion-dia-internacional-amistad_23-2150515715.jpg',
       description: 'En este proyecto exploraremos historias sobre la amistad para fomentar la empatía y la solidaridad en la infancia. A través de narraciones emotivas y personajes entrañables, los niños descubrirán el valor del compañerismo, el trabajo en equipo y la importancia de apoyarse mutuamente. Cada cuento estará diseñado para provocar reflexión y diálogo tanto en casa como en el aula.'
     },
     {
@@ -46,7 +46,7 @@ export class HomeComponent  implements OnInit {
       author: 'Lucía Fernández',
       date: '12/05/2025',
       applicationDate: '12/05/2025',
-      imageSrc: 'https://www.magisnet.com/wp-content/uploads/2019/02/storytelling.jpg',
+      imageSrc: 'https://static.vecteezy.com/system/resources/previews/023/967/493/large_2x/world-day-for-cultural-diversity-for-dialogue-and-development-abstract-illustration-generative-ai-photo.jpg',
       description: 'Este proyecto tiene como objetivo desarrollar cuentos ilustrados que promuevan la inclusión y el respeto por las diferencias. A través de relatos protagonizados por niños y niñas de diferentes culturas, capacidades y contextos, se busca construir una visión más abierta y comprensiva del mundo. El enfoque será pedagógico y emocional, incorporando actividades complementarias para padres y educadores.'
     }
   ];
